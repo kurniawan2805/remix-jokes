f893b2a342b5c36ab28f6bf4ea84bb8c6a5e64c3
kurniawan2805 <42286406+kurniawan2805@users.noreply.github.com>
2022-09-21 11:01:18 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-xlun6o
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-xlun6o)
---
+++ app/root.tsx
@@ -0,0 +1,30 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react';
+import type { MetaFunction } from '@remix-run/node';
+
+export const meta: MetaFunction = () => {
+  return { title: 'New Cool Remix App' };
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
+      </body>
+    </html>
+  );
+}
